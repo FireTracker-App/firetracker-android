26191d370ded96e14f6eeaab09fae52a0f006592
Techdoodle <chris@techchrism.me>
2020-11-06 23:46:04 -0800

    add support for loading reported fires

--- app/src/main/java/me/techchrism/firetracker/NetworkManager.kt
+++ app/src/main/java/me/techchrism/firetracker/NetworkManager.kt
@@ -3,10 +3,12 @@ package me.techchrism.firetracker
 import android.content.Context
 import com.android.volley.Request
 import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import me.techchrism.firetracker.firedata.CalFireData
 import me.techchrism.firetracker.firedata.FireData
+import me.techchrism.firetracker.firedata.ReportedFireData
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -20,13 +22,45 @@ class NetworkManager
     lateinit var onFireRemoved: (FireData) -> Unit
 
     init {
-        loadFireData()
+        loadCalFireData()
+        loadReportedFireData()
+    }
+
+    private fun loadReportedFireData() {
+        val reportedFireDataRequest = JsonArrayRequest(
+            Request.Method.GET,
+            "https://firetracker.techchrism.me/markers",
+            null,
+            { response ->
+                val format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+
+                // Iterate through the reports in the api
+                for (i in 0 until response.length()) {
+                    val report = response.getJSONObject(i)
+                    val id = UUID.nameUUIDFromBytes(report.getString("_id").toByteArray())
+                    val fireData = ReportedFireData(
+                        id,
+                        report.getDouble("latitude"),
+                        report.getDouble("longitude"),
+                        format.parse(report.getString("reported"))!!
+                    )
+                    incidentSet.add(fireData)
+                    if(this::onNewFire.isInitialized) {
+                        onNewFire(fireData)
+                    }
+                }
+            },
+            { error ->
+                // TODO: Handle error
+            }
+        )
+        requestQueue.add(reportedFireDataRequest)
     }
 
     /**
      * Loads the fire data from the API and, if the map is ready, displays it
      */
-    private fun loadFireData() {
+    private fun loadCalFireData() {
         val fireDataRequest = JsonObjectRequest(
             Request.Method.GET,
             "https://www.fire.ca.gov/umbraco/Api/IncidentApi/GetIncidents",
