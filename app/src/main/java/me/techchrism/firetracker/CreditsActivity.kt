9830b5186aa46d8ada6f24cdaa6dc13a86fe6c54
BenTischhauser <BenTischhauser@gmail.com>
2020-11-22 11:40:55 -0800

    Committed all of the files I failed to commit last time... woops!

---
+++ app/src/main/java/me/techchrism/firetracker/CreditsActivity.kt
@@ -0,0 +1,21 @@
+package me.techchrism.firetracker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class CreditsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_credits)
+
+        // Set up button functionality to return to main fire map.
+        val returnButton = findViewById<Button>(R.id.returnBtnAbout)
+
+        returnButton?.setOnClickListener() {
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
