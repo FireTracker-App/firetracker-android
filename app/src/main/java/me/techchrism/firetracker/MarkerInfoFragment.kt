1717f5cbc1744edc77570179368f661924366e41
Techdoodle <chris@techchrism.me>
2020-12-01 14:56:29 -0800

    add remaining labels and data for cal fires

--- app/src/main/java/me/techchrism/firetracker/MarkerInfoFragment.kt
+++ app/src/main/java/me/techchrism/firetracker/MarkerInfoFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import me.techchrism.firetracker.firedata.CalFireData
 import me.techchrism.firetracker.firedata.FireData
 import me.techchrism.firetracker.firedata.ReportedFireData
+import java.text.DateFormat
 
 class MarkerInfoFragment(private val fireData: FireData) : Fragment() {
 
@@ -26,13 +28,31 @@ class MarkerInfoFragment(private val fireData: FireData) : Fragment() {
 
         if(fireData is ReportedFireData) {
             view.findViewById<TextView>(R.id.marker_title).text = "Reported Fire"
+            view.findViewById<LinearLayout>(R.id.marker_location).visibility = View.GONE
+            view.findViewById<LinearLayout>(R.id.marker_burned).visibility = View.GONE
+            view.findViewById<LinearLayout>(R.id.marker_started).visibility = View.GONE
+            view.findViewById<LinearLayout>(R.id.marker_contained).visibility = View.GONE
         }
         else if(fireData is CalFireData) {
             view.findViewById<TextView>(R.id.marker_title).text = fireData.name
+            view.findViewById<TextView>(R.id.marker_location_text).text = fireData.location
+            if(fireData.acresBurned == null) {
+                view.findViewById<LinearLayout>(R.id.marker_burned).visibility = View.GONE
+            }
+            else {
+                view.findViewById<TextView>(R.id.marker_burned_text).text = fireData.acresBurned.toString()
+            }
+            view.findViewById<TextView>(R.id.marker_started_text).text = DateFormat.getDateInstance().format(fireData.started)
+            if(fireData.percentContained == null) {
+                view.findViewById<LinearLayout>(R.id.marker_contained).visibility = View.GONE
+            }
+            else {
+                view.findViewById<TextView>(R.id.marker_contained_text).text = fireData.percentContained.toString() + "%"
+            }
         }
 
         if(fireData.description == null) {
-            view.findViewById<TextView>(R.id.marker_description).visibility = View.GONE
+            view.findViewById<LinearLayout>(R.id.marker_description).visibility = View.GONE
         }
         else {
             view.findViewById<TextView>(R.id.marker_description_text).text = fireData.description
