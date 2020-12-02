cb87cb1807feb78ec798cef479a652f90a4a7934
Alex Hadland <alexander.hadland400@myci.csuci.edu>
2020-12-01 19:18:12 -0800

    Added communication between MapsActivity and EditMarker

--- app/src/main/java/me/techchrism/firetracker/EditMarkerActivity.kt
+++ app/src/main/java/me/techchrism/firetracker/EditMarkerActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 
 class EditMarkerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,9 +14,25 @@ class EditMarkerActivity : AppCompatActivity() {
         // Set up button functionality to return to main fire map.
         val returnButton = findViewById<Button>(R.id.returnBtnAbout)
 
+
         returnButton?.setOnClickListener() {
-            val intent = Intent(this, MapsActivity::class.java)
-            startActivity(intent)
+            saveDetails()
+//            val intent = Intent(this, MapsActivity::class.java)
+//            startActivity(intent)
         }
     }
+
+    fun saveDetails() {
+        val userDescription = findViewById<EditText>(R.id.editTextTextMultiLine).text.toString()
+
+        val result = Intent()
+
+        result.putExtra("descriptionKey", userDescription)
+        result.putExtra("requestCode", 1)
+        result.putExtra("resultCode", RESULT_OK)
+
+        setResult(RESULT_OK, result)
+        finish()
+    }
 }
+
