a3d7e006a925f7e9b215a227cafce34c3ce07662
BenTischhauser <BenTischhauser@gmail.com>
2020-12-01 20:08:29 -0800

    - Modified AndroidManifest.xml to rid errors.

--- app/src/main/java/me/techchrism/firetracker/NewMarkerSetupActivity.kt
+++ app/src/main/java/me/techchrism/firetracker/NewMarkerSetupActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 
-class EditMarkerActivity : AppCompatActivity() {
+class NewMarkerSetupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_marker_setup)
